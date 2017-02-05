e88c01f557b786aa67b784b053c3e98fb255fd71
Sebastian Plamauer <oeplse@gmail.com>
2017-02-05 16:03:41 +0100

    Fix accelerometer noise, Implement Gyro

--- bmg160.py
+++ bmg160.py
@@ -25,50 +25,134 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-import machine
+#from machine import I2C
+from nativeio import I2C
+from time import sleep
 
 # from stackoverflow J.F. Sebastian
 def _twos_comp(val, bits=8):
-    """compute the 2's complement of int val with bits"""
+    '''
+    compute the 2's complement of int val with bits
+    '''
     if (val & (1 << (bits - 1))) != 0: # if sign bit is set
         val = val - (1 << bits)        # compute negative value
     return val                         # return positive value as is
 
 
 class BMG160():
-    '''gyroscope'''
+    '''
+    Class for BMG160 gyroscope
+    '''
 
-    def __init__(self, i2c):
+    def __init__(self, i2c: I2C, addr):
+        '''
+        Initializes with an i2c object as argument.
+        Checks if device is connected.
+        Sets range to 16g and filter bandwith to 128Hz.
+        '''
 
-        if type (i2c) == machine.I2C:
+        self.buf = bytearray(64)
+
+        if type (i2c) == I2C:
             self.i2c = i2c
-        else:
-            raise TypeError('passed argument is not an I2C object')
         self.gyro_addr = 0x68
-        try:
-            self.chip_id = i2c.readfrom_mem(self.gyro_addr, 0x00, 1)[0]
-        except OSError:
-            self.acc_addr = 0x69
-            try:
-                self.chip_id = i2c.readfrom_mem(self.gyro_addr, 0x00, 1)[0]
-            except OSError:
-                raise OSError('no BMG160 connected')
+        self.chip_id = self._readfrom_mem(self.gyro_addr, 0x00, 1)[0]
+        self.set_range(125)      # default range 2000deg/s
+        self.set_filter_bw(116)    # default filter bandwith 64Hz
+        self.compensation()
+
+    def _readfrom_mem(self, dev_addr, addr, no):
+        self.i2c.readfrom_into(dev_addr, self.buf)
+        return self.buf[addr:addr+no]
+
+    def _writeto_mem(self, dev_addr, addr, byte):
+        self.i2c.readfrom_into(dev_addr, self.buf)
+        self.buf[addr] = int(byte)
+        self.i2c.readfrom_into(dev_addr, self.buf)
 
     def _read_gyro(self, addr):
-        """return accel data from addr"""
-        LSB, MSB = self.i2c.readfrom_mem(self.gyro_addr, addr, 2)
+        '''
+        return gyro data from addr
+        '''
+        LSB, MSB = self._readfrom_mem(self.gyro_addr, addr, 2)
         LSB = _twos_comp(LSB)
         MSB = _twos_comp(MSB)
-        return 2000*(LSB + (MSB<<8))/(2**15)
+        return (LSB + (MSB<<4))*self._resolution
 
-    def x(self):
+    def set_range(self, gyro_range: int):
+        '''
+        Sets the gyro range to 125, 250, 500, 1000 or 2000deg/s.
+        '''
+        ranges = {125:0x04, 250:0x03, 500:0x02, 1000:0x01, 2000:0x00}
+        try:
+            range_byte = ranges[gyro_range]
+        except KeyError:
+            raise ValueError('invalid range, use 125, 250, 500, 1000 or 2000')
+        self._writeto_mem(self.gyro_addr, 0x0F, range_byte)
+        self._resolution = (2*gyro_range)/2**16
+
+    def get_range(self) -> int:
+        '''
+        Returns the gyro range.
+        '''
+        return int(2000/2**self._readfrom_mem(self.gyro_addr, 0x0F, 1)[0])
+
+    def set_filter_bw(self, freq: int):
+        '''
+        Sets the filter bandwith to 12, 23, 32, 47, 64, 116, 230 or 523Hz.
+        '''
+        freqs = {12:0x05, 23:0x04, 32:0x07, 47: 0x03, 64:0x06, 116:0x02, 230:0x01, 523:0x00}
+        try:
+            freq_byte = freqs[freq]
+        except:
+            raise ValueError('invalid filter bandwith, use 12, 23, 32, 47, 64, 116, 230 or 523')
+        self._writeto_mem(self.gyro_addr, 0x10, freq_byte)
+
+    def get_filter_bw(self) -> int:
+        '''
+        Returns the filter bandwith.
+        '''
+        return {0:523,1:230,2:116,3:47,4:23,5:12,6:64,7:32}[self._readfrom_mem(self.gyro_addr, 0x10, 1)[0]]
+
+    def compensation(self, active=None) -> bool:
+        '''
+        With no arguments passed, runs fast compensation.
+        With boolean argument passe, activates or deactivates slow compensation.
+        '''
+        gyro_range = self.get_range()
+        self.set_range(125)
+        self._writeto_mem(self.gyro_addr, 0x37, 0x21) # settings x0y0z1 10Hz
+        self._writeto_mem(self.gyro_addr, 0x36, 0x80) # reset
+        if active is None:  # trigger fast comp
+            self._writeto_mem(self.gyro_addr, 0x36, 0x00) # deactivate slow comp
+            active = False
+            #print(self._readfrom_mem(self.gyro_addr, 0x36, 1))
+            self._writeto_mem(self.gyro_addr, 0x36, 0x20) # x
+            sleep(0.1)
+            #print(self._readfrom_mem(self.gyro_addr, 0x36, 1))
+            self._writeto_mem(self.gyro_addr, 0x36, 0x40) # y
+            sleep(0.1)
+            #print(self._readfrom_mem(self.gyro_addr, 0x36, 1))
+            self._writeto_mem(self.gyro_addr, 0x36, 0x60) # z
+            sleep(0.1)
+            #print(self._readfrom_mem(self.gyro_addr, 0x36, 1))
+        elif active:        # activate slow comp
+            self._writeto_mem(self.gyro_addr, 0x36, 0x07)
+        elif not active:    # deactivate slow comp
+            self._writeto_mem(self.gyro_addr, 0x36, 0x00)
+        else:
+            raise TypeError('pass a boolean or no argument')
+        self.set_range(gyro_range)
+        return active
+
+    def x(self) -> float:
         return self._read_gyro(0x02)
 
-    def y(self):
+    def y(self) -> float:
         return self._read_gyro(0x04)
 
-    def z(self):
+    def z(self) -> float:
         return self._read_gyro(0x06)
 
-    def xyz(self):
+    def xyz(self) -> tuple:
         return (self.x(), self.y(), self.z())
--- main.py
+++ main.py
@@ -1,28 +1,15 @@
-# main.py -- put your code here!
+from nativeio import I2C
+from board import *
+from time import sleep
 
-from machine import I2C, Pin
-from bmx055 import BMX055
-from attitude import angles
-import time
+from bma2x2 import BMA2X2
+from bmg160 import BMG160
 
-# for atsamd21
-i2c_bus = I2C(scl=Pin.board.SCL, sda=Pin.board.SDA, freq=400000)
-i2c_bus.init()
+i2c = I2C(SCL, SDA)
+i2c.try_lock()
+addresses = [ int(address) for address in i2c.scan() ]
 
-imu = BMX055(i2c_bus)
+print('found devices on', addresses)
 
-print(imu.accel.i2c.readfrom_mem(imu.accel.acc_addr, 0, 63))
-
-print('temperature:', imu.accel.temperature())
-print('resolution:', imu.accel._resolution)
-print('accel:', imu.accel.xyz())
-print('set range 2:', imu.accel.set_range(2))
-print('set filter 128:', imu.accel.set_filter_bw(128))
-
-t_end = time.ticks_ms() + 100*1000
-while time.ticks_ms() < t_end:
-    imu_data = imu.accel.xyz()
-    print(imu_data, angles(imu_data))
-    time.sleep_ms(1000)
-
-i2c_bus.deinit()
+accel = BMA2X2(i2c, 25)
+gyro = BMG160(i2c, 17)
